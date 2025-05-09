f4a057b9179a70b39d395d9a78d341a580fa858f
Adarsh Tiwari <87087693+tiwari-adarsh@users.noreply.github.com>
2025-05-09 11:21:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycby1KR6lzpox-qnR8Jw9-Eg7NUFt5CZXmMUL1lroabNdjMa_NqjOIkcTB4MChUb80oMl/exec';
 
+
 fnSubmit = () => {
     var name = document.getElementById('Name').value;
     var email = document.getElementById('EmailId').value;
@@ -10,12 +11,24 @@ fnSubmit = () => {
     formdata.append("email",email);
     formdata.append("comments",comment);
 
-    fetch(scriptURL,{method:"POST",body:formdata})
-        .then(async (response) => { if (response.ok) {alert("Thank You for Reaching Out "+ name + " ji !");
-                                                      const result = await response.text();
-                                                      console.log(result);
-                                                     }
-                                  })
-        .catch(error => { alert("currently unable to send your message please try later.");
-                         console.error("Detailed error:", error);});
+    fetch(scriptURL,{method:"POST",body:formdata}).then(async (response) => {
+  if (!response.ok) {
+    throw new Error("HTTP status " + response.status);
+  }
+  const result = await response.text();
+  alert("Success: " + result);
+})
+.catch(error => {
+  alert("Error: " + error.message);
+  console.error("Detailed error:", error);
+});
+       
 }
+
+ //.then(async (response) => { if (response.ok) {alert("Thank You for Reaching Out "+ name + " ji !");
+//                                                      const result = await response.text();
+//                                                     console.log(result);
+//                                                     }
+//                                  })
+//        .catch(error => { alert("currently unable to send your message please try later.");
+//                        console.error("Detailed error:", error);});
