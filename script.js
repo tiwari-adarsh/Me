72ffc18423fa51ed6911826ab7df871c9fee34c4
Adarsh Tiwari <87087693+tiwari-adarsh@users.noreply.github.com>
2025-05-13 14:55:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,6 +27,6 @@ fnSubmit = () => {
 
 function isValidEmail(email) {
   const strictEmailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  return emailRegex.test(email);
+  return strictEmailRegex.test(email);
 }
 
