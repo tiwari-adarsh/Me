7dea242866c7b79a7bd6fe0a86aa2d870cf0c599
Adarsh Tiwari <87087693+tiwari-adarsh@users.noreply.github.com>
2025-05-14 11:57:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,28 +2,47 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycby1KR6lzpox-qnR8Jw9-E
 
 
 fnSubmit = () => {
+    document.querySelector("#submitBtn").disabled = true;
+    document.querySelector("#submitBtn").innerText = "Sending...";
+
     var name = document.getElementById('Name').value;
     var email = document.getElementById('EmailId').value;
     var comment = document.getElementById('Comment').value;
 
-    if (!isValidEmail(email)) {     alert("❌ Please enter a valid email address.");
-     return;
-    }
+   
+    
     if(name != "" && email != "" && comment != "" ){
+         if (!isValidEmail(email)) {     
+             alert("❌ Please enter a valid email address.");
+              return;
+         }
+        
     const formdata = new FormData();
     formdata.append("name",name);
     formdata.append("email",email);
     formdata.append("comments",comment);
 
-    fetch(scriptURL, {
-          method: 'POST',
-          body: formdata,
-          mode: 'no-cors'
-          }).then(() => {    alert("✅ Your message has been received!\n\nThanks for taking the time to connect. We’ll get back to you soon — hopefully at the right email 😊");  })
-            .catch(error => { console.error("Fetch error:", error);
-                              alert("⚠️ Something went wrong. Please try again later.");});
-     }  
+    sendFormData(formData)
+    .then(() => {    alert("✅ Your message has been received!\n\nThanks for taking the time to connect. We’ll get back to you soon — hopefully at the right email 😊");  })
+    .catch(error => {    console.error("Form submission error:", error);   
+                         alert("⚠️ Something went wrong. Please try again later.");  
+                    });      
+    }  
 }
+
+
+sendFormData = (formData) =>{
+    return new Promise((resolve, reject) => {
+   fetch('https://script.google.com/macros/s/AKfycby1KR6lzpox-qnR8Jw9-Eg7NUFt5CZXmMUL1lroabNdjMa_NqjOIkcTB4MChUb80oMl/exec', {
+      method: 'POST',
+      body: formData,
+      mode: 'no-cors'
+    })
+    .then(() => resolve())
+    .catch(error => reject(error));
+  });
+}
+
 
 function isValidEmail(email) {
   const strictEmailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
